2122901cbd707c4d7c5a4dde7ef2af5a5d4884fd
Brendan Taylor <45243880+Brend7n@users.noreply.github.com>
2019-05-03 11:21:37 -0400

    Add files via upload

---
+++ src/main/java/com/e/quizapp/QuizContract.kt
@@ -0,0 +1,18 @@
+package com.e.quizapp
+
+import android.provider.BaseColumns
+
+object QuizContract {
+
+    class QuestionsTable : BaseColumns {
+        companion object {
+            val TABLE_NAME = "quiz_questions"
+            val COLUMN_QUESTION = "question"
+            val COLUMN_OPTION1 = "option1"
+            val COLUMN_OPTION2 = "option2"
+            val COLUMN_OPTION3 = "option3"
+            val COLUMN_ANSWER_NR = "answer_nr"
+            //val _ID = null
+        }
+    }
+}
